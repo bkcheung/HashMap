252550e49d63dd330c86d88f4132b7919f88584f
bkcheung <b.cheung@hotmail.com>
2024-03-06 15:03:25 -0500

    Update node modules and add git ignore

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import linkedList from "@bkcheung/linked-list";
 let buckets = 16;
 let map = hashMap();
 map.set('hello','all');
@@ -18,7 +19,49 @@ function hashMap(){
             return hashCode;
         },
         set(key, value){
-            this.hash_map[this.hash(key)]=value;
+            let hashKey = this.hash(key);
+            let bucket = this.hash_map[hashKey];
+            let newNode = node();
+            newNode.setKeyValue(key, value);
+            if(bucket!==undefined){
+                if(bucket.key === key){
+                    console.log('exists!');
+                    bucket.setKeyValue(key, value); //overwrite
+                    return;
+                }
+                while(bucket.next!==undefined){
+                    if(bucket.key === key){
+                        console.log('exists!');
+                        bucket.setKeyValue(key, value); //overwrite
+                        return;
+                    } else{
+                        bucket = bucket.next;
+                    }
+                }
+                bucket.next = newNode;
+            }
+            bucket = newNode;
+            console.log(bucket);
+        }        
+    }
+}
+function linkedList(){
+    return{
+        head,
+        
+    }
+}
+function node(){
+    return{
+        key: null,
+        value: null,
+        next: null,
+        setKeyValue(key, value){
+            this.key = key;
+            this.value = value;
+        },
+        setNext(next){
+            this.next = next;
         }
     }
 }
